0380cee46665c71a5d90f2ea981fcd75d7c76db5
TPODAvia <thepowerofdarknes2000@gmail.com>
2024-01-11 11:30:57 +0100

    Repo update

--- sensors/ros-mpu9250-ahrs/mpu9250-driver/scripts/check.py
+++
@@ -1,8 +0,0 @@
-import smbus2
-
-bus = smbus2.SMBus(1) # Create an SMBus instance
-AK8963_ADDRESS = 0x0C
-WHO_AM_I = 0x00
-
-who_am_i = bus.read_byte_data(AK8963_ADDRESS, WHO_AM_I)
-print(hex(who_am_i)) # It should print: 0x48
