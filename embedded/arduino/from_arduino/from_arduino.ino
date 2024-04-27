2543c5466dc4efa51aa8f16315998d4274679217
TPODAvia <thepowerofdarknes2000@gmail.com>
2024-04-27 18:04:57 +0300

    update arduino software

--- embedded/arduino/from_arduino/from_arduino.ino
+++ embedded/arduino/from_arduino/from_arduino.ino
@@ -8,7 +8,16 @@ const uint8_t addresses[numSensors] = {0x40, 0x41, 0x42, 0x43, 0x40, 0x41, 0x42,
 TwoWire* wires[numSensors] = {&Wire, &Wire, &Wire, &Wire, &Wire1, &Wire1, &Wire1, &Wire1};
 
 // Array to hold sensor instances
-INA3221 sensors[numSensors];
+INA3221 sensors[numSensors] = {
+    INA3221(addresses[0], wires[0]),
+    INA3221(addresses[1], wires[1]),
+    INA3221(addresses[2], wires[2]),
+    INA3221(addresses[3], wires[3]),
+    INA3221(addresses[4], wires[4]),
+    INA3221(addresses[5], wires[5]),
+    INA3221(addresses[6], wires[6]),
+    INA3221(addresses[7], wires[7])
+};
 
 // Multiplexer pins
 const int muxSIG = A0;
@@ -23,7 +32,6 @@ Servo servos[20];
 int servoPins[20] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
 int positions[20];
 bool DEBUG = true;
-int analog_n = 12;
 
 void setup() {
   Serial.begin(115200);
@@ -66,32 +74,11 @@ void setup() {
   }
   pinMode(13, OUTPUT);
   analogReadResolution(12);
+
+  delay(5000); // Adjust delay as needed
 }
 
-void loop() {
-  // INA3221 sensor readings
-  Serial.println("\nReading INA3221 Sensors");
-  for (int i = 0; i < numSensors; i++) {
-    Serial.println("------------------------");
-    readSensor(sensors[i]);
-  }
-
-  // Multiplexer operations
-  SetMuxChannel(5);
-  int val = analogRead(muxSIG);
-  Serial.println(val);
-
-  // Servo operations based on serial input
-  if (Serial.available() > 0) {
-    String inputString = Serial.readString();
-    // Process inputString to update servo positions...
-    // (Include the servo processing code here)
-  }
-
-  delay(1000); // Adjust delay as needed
-}
-
-void readSensor(INA3221 &sensor) {
+void readSensor_all(INA3221 &sensor) {
   Serial.println("\nCHAN\tBUS\tSHUNT\tCURRENT\tPOWER");
   for (int ch = 0; ch < 3; ch++) {
     Serial.print(ch);
@@ -107,9 +94,103 @@ void readSensor(INA3221 &sensor) {
   }
 }
 
+void readSensor(INA3221 &sensor) {
+  for (int ch = 0; ch < 3; ch++) {
+    Serial.print(sensor.getCurrent_mA(ch), 3);
+  }
+}
+
+
 int SetMuxChannel(byte channel) {
   digitalWrite(muxS0, bitRead(channel, 0));
   digitalWrite(muxS1, bitRead(channel, 1));
   digitalWrite(muxS2, bitRead(channel, 2));
   digitalWrite(muxS3, bitRead(channel, 3));
+}
+
+
+void loop() {
+
+ digitalWrite(13, LOW);
+ if (Serial.available() > 0) {
+    digitalWrite(13, HIGH);
+    // Read the incoming string
+    String inputString = Serial.readString();
+
+    // Split the string into positions
+    int index = 0;
+    int from = 0;
+    int to = inputString.indexOf(',');
+    bool okay = false;
+    while (to != -1) {
+      positions[index++] = inputString.substring(from, to).toInt();
+      from = to + 1;
+      to = inputString.indexOf(',', from);
+    }
+    positions[index] = inputString.substring(from).toInt(); // Last position
+
+    // Update servo positions and prepare feedback string
+    String feedback;
+    feedback += ",";
+    for (int i = 0; i <= index; i++) { // Note: Use <= index to iterate through all positions including the last
+      if (index != 19) {
+        if (DEBUG) {
+          Serial.println("There're should be 20 inputs");
+        }
+        break;
+      }
+      if (!((positions[i] >= 0) && (positions[i] <= 180))) {
+        if (DEBUG) {
+          Serial.println("Input string position are not in [0,180]");
+        }
+        break;
+      } else {
+        okay = true;
+      }
+    }
+
+    if (okay == true) {
+      for (int i = 0; i <= index; i++) { // Note: Use <= index to iterate through all positions including the last
+        servos[i].write(positions[i]); // Move each servo to its new position
+        feedback += positions[i];
+        if (i < index) {
+          feedback += ",";
+        } else {
+          feedback += ";";
+        }
+      }
+    }
+
+    // INA3221 sensor readings
+    for (int i = 0; i < numSensors; i++) {
+      for (int ch = 0; ch < 3; ch++) {
+        feedback += String(sensors[i].getCurrent_mA(ch));
+        if (i < numSensors) {
+          feedback += ",";
+        } else {
+          feedback += ";";
+        }
+      }
+    }
+
+    for (int i = 0; i < 16; i++) { // Assuming A0 and multiplexer
+        // Multiplexer operations
+        SetMuxChannel(i);
+        int val = analogRead(muxSIG);
+        Serial.println(val);
+        feedback += String(val);
+        if (i < 16) {
+          feedback += ",";
+        }
+    }
+    for (int i = 1; i < 5; i++) { // Assuming A1 to A4
+        int sensorValue = analogRead(i);
+        feedback += String(sensorValue);
+        if (i < 4) {
+          feedback += ",";
+        }
+    }
+    Serial.println(feedback);
+ }
+
 }
